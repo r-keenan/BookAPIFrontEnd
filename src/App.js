d51c825224cc0cab3e203611b337c7d95c6d3c3a
rkeenan <thero@DESKTOP-T4F6H9S>
2022-02-10 11:21:12 -0600

    Added in Axios. console logging the object.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import axios from "axios";
 import "./App.css";
 
+const apiURL = "https://localhost:7068/api/Books/";
+const bookTitle = document.querySelector("bookTitle");
+
 function App() {
+  axios
+    .get(apiURL)
+    .then(function (response) {
+      console.log(response.data);
+      //bookTitle.innerHTML = response.data.forms.title;
+    })
+    .catch(function (error) {
+      console.log(`Error: ${error}`);
+      //bookTitle.innerHTML = `An error has occurred: ${error}`;
+    });
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <h1>Axios Demo</h1>
+      <label htmlFor="BookTitle">Book Title</label>
+      <input type="text" name="BookTitle" className="bookTitle" />
     </div>
   );
 }
